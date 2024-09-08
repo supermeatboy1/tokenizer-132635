5a1287ff953d247c31dc6a54e62ca1c54d01e61e
supermeatboy1 <jmagunod@mcm.edu.ph>
2024-09-08 19:53:52 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,120 +1,20 @@
+use crate::tokenizer::TokensResult;
+use egui::{FontId, RichText};
 use std::error::Error;
 
 use eframe::egui;
 
-#[derive(Debug)]
-struct Token<'a> {
-    part: &'a str,
-}
-
-impl Token<'_> {
-    fn display_granular(&self) {
-        println!(
-            "Token: {:?} -> {:?}",
-            self.part,
-            self.part.chars().into_iter().collect::<Vec<char>>()
-        );
-    }
-}
-
-fn is_alphabetic(character: char) -> bool {
-    (character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z')
-}
-
-fn is_valid_domain_char(character: char) -> bool {
-    is_alphabetic(character) || (character >= '0' && character <= '9') || character == '-'
-}
-
-fn validate_protocol(protocol_str: &str) -> Result<&str, Box<dyn Error>> {
-    let end_index: usize = protocol_str.len() - 1;
-    for (current_index, c) in protocol_str.chars().enumerate() {
-        if (current_index != end_index && !is_alphabetic(c))
-            || (current_index == end_index && c != ':')
-        {
-            return Err("This is not a valid protocol.".into());
-        }
-    }
-    return Ok(&protocol_str[..end_index - 1]);
-}
-
-fn validate_hostname(hostname_str: &str) -> Result<&str, Box<dyn Error>> {
-    if hostname_str.len() == 0 {
-        return Err("This is not a valid hostname.".into());
-    }
-    let mut periods: usize = 0;
-    let mut previous_is_period = false;
-    for (current_index, c) in hostname_str.chars().enumerate() {
-        if c == '.' {
-            if previous_is_period {
-                return Err("This is not a valid hostname.".into());
-            }
-            periods += 1;
-            previous_is_period = true;
-            continue;
-        }
-        previous_is_period = false;
-        if (current_index == 0 && c == '-') || (current_index != 0 && !is_valid_domain_char(c)) {
-            return Err("This is not a valid hostname.".into());
-        }
-    }
-    if periods > 2 {
-        return Err("This is not a valid hostname.".into());
-    }
-    Ok(&hostname_str)
-}
-
-fn validate_tokens(tokens: Vec<Token>) -> Result<Vec<Token>, Box<dyn Error>> {
-    if tokens.len() < 2 {
-        return Err("Invalid URL.".into());
-    }
-    for token in &tokens[2..] {
-        println!("Files / Directories: {:?}", token);
-    }
-    Ok(tokens)
-}
-
-fn extract_tokens(input_str: &str) -> Vec<Token> {
-    let mut tokens: Vec<Token> = Vec::new();
-    let mut start_index: usize = 0;
-    for (index, c) in input_str.chars().enumerate() {
-        if c == '/' {
-            let extracted: &str = &input_str[start_index..index];
-            // skip empty tokens
-            if !extracted.is_empty() {
-                tokens.push(Token { part: extracted });
-            }
-            start_index = index + 1;
-        }
-    }
-    let last_part: &str = &input_str[start_index..];
-    if !last_part.is_empty() {
-        tokens.push(Token { part: last_part });
-    }
-    tokens
-}
+mod str_checker;
+mod tokenizer;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let url: &str = "https://mcm.edu.ph/test_directory/test_subdirectory/test_page.php";
-    let tokens: Vec<Token> = validate_tokens(extract_tokens(url))?;
-
-    let protocol: &str = validate_protocol(tokens.get(0).unwrap().part)?;
-    let hostname: &str = validate_hostname(tokens.get(1).unwrap().part)?;
-
-    println!("Tokens: {:#?}", tokens);
-
-    println!("Protocol: {}", protocol);
-    println!("Hostname: {}", hostname);
-
-    for token in tokens.iter() {
-        token.display_granular();
-    }
-
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([853.0, 480.0]),
+        viewport: egui::ViewportBuilder::default().with_inner_size([1000.0, 650.0]),
+        centered: true,
         ..Default::default()
     };
     eframe::run_native(
-        "My egui app!",
+        "Tokenizer",
         options,
         Box::new(|_cc| Ok(Box::<GraphicApp>::default())),
     )?;
@@ -122,18 +22,144 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-struct GraphicApp {}
+struct GraphicApp {
+    url: String,
+
+    protocol: String,
+    host: String,
+    directories: String,
+    filename: String,
+    query: String,
+
+    words: String,
+    punctuations: String,
+    numbers: String,
+    alphanumeric: String,
+
+    is_valid_url: String,
+
+    granular: String,
+}
 
 impl Default for GraphicApp {
     fn default() -> Self {
-        Self {}
+        Self {
+            url: String::default(),
+
+            protocol: String::default(),
+            host: String::default(),
+            directories: String::default(),
+            filename: String::default(),
+            query: String::default(),
+
+            words: String::default(),
+            punctuations: String::default(),
+            numbers: String::default(),
+            alphanumeric: String::default(),
+
+            is_valid_url: String::default(),
+
+            granular: String::default(),
+        }
     }
 }
 
 impl eframe::App for GraphicApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("hi sir josh and josh lechoncito");
+            ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
+                ui.label(
+                    RichText::new("URL Tokenizer")
+                        .font(FontId::proportional(40.0))
+                        .strong(),
+                );
+            });
+
+            ui.add_space(20.0);
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                ui.horizontal(|ui| {
+                    ui.label(RichText::new("Input:").font(FontId::proportional(20.0)));
+                    let response = ui.add_sized(
+                        [ui.available_width(), 30.0],
+                        egui::TextEdit::singleline(&mut self.url),
+                    );
+
+                    // Execute if Enter key is pressed.
+                    if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
+                        let result: TokensResult = TokensResult::new(&self.url);
+
+                        self.protocol = result.protocol.unwrap_or("").to_string();
+                        self.host = result.host.unwrap_or("").to_string();
+                        self.directories = format!("{:#?}", result.directories);
+                        self.filename = result.filename.unwrap_or("").to_string();
+                        self.query = result.query.unwrap_or("").to_string();
+
+                        self.words = format!("{:#?}", result.words);
+                        self.numbers = format!("{:#?}", result.numbers);
+                        self.alphanumeric = format!("{:#?}", result.alphanumeric);
+                        self.punctuations = format!("{:#?}", result.punctuations);
+
+                        self.granular = result.granular();
+
+                        self.is_valid_url = if result.is_valid_url {
+                            "The input is a valid URL.".to_string()
+                        } else {
+                            "The input is not a valid URL.".to_string()
+                        };
+                    }
+                });
+
+                ui.add_space(10.0);
+                ui.label(&self.is_valid_url);
+                ui.add_space(10.0);
+
+                ui.columns(2, |columns| {
+                    columns[0].label(RichText::new("Protocol:").font(FontId::proportional(20.0)));
+                    columns[0].add(egui::TextEdit::multiline(&mut self.protocol.as_str()));
+                    columns[0].add_space(20.0);
+                    columns[1].label(RichText::new("Word:").font(FontId::proportional(20.0)));
+                    columns[1].add(egui::TextEdit::multiline(&mut self.words.as_str()));
+                    columns[1].add_space(20.0);
+
+                    columns[0].label(RichText::new("Host:").font(FontId::proportional(20.0)));
+                    columns[0].add(egui::TextEdit::multiline(&mut self.host.as_str()));
+                    columns[0].add_space(20.0);
+                    columns[1]
+                        .label(RichText::new("Punctuation:").font(FontId::proportional(20.0)));
+                    columns[1].add(egui::TextEdit::multiline(&mut self.punctuations.as_str()));
+                    columns[1].add_space(20.0);
+
+                    columns[0]
+                        .label(RichText::new("Directories:").font(FontId::proportional(20.0)));
+                    columns[0].add(egui::TextEdit::multiline(&mut self.directories.as_str()));
+                    columns[0].add_space(20.0);
+                    columns[1].label(RichText::new("Number:").font(FontId::proportional(20.0)));
+                    columns[1].add(egui::TextEdit::multiline(&mut self.numbers.as_str()));
+                    columns[1].add_space(20.0);
+
+                    columns[0].label(RichText::new("Filename:").font(FontId::proportional(20.0)));
+                    columns[0].add(egui::TextEdit::multiline(&mut self.filename.as_str()));
+                    columns[0].add_space(20.0);
+                    columns[1]
+                        .label(RichText::new("Alphanumeric:").font(FontId::proportional(20.0)));
+                    columns[1].add(egui::TextEdit::multiline(&mut self.alphanumeric.as_str()));
+                    columns[1].add_space(20.0);
+
+                    columns[0].label(RichText::new("Query:").font(FontId::proportional(20.0)));
+                    columns[0].add(egui::TextEdit::multiline(&mut self.query.as_str()));
+                    columns[0].add_space(20.0);
+                    /*columns[1]
+                        .label(RichText::new("Other Tokens:").font(FontId::proportional(20.0)));
+                    columns[1].add(egui::TextEdit::multiline(&mut self.others.as_str()));
+                    columns[1].add_space(20.0);*/
+                });
+
+                ui.label(RichText::new("Granular breakdown:").font(FontId::proportional(20.0)));
+                ui.add_sized(
+                    [ui.available_width(), 256.0],
+                    egui::TextEdit::multiline(&mut self.granular.as_str()),
+                );
+            });
         });
     }
 }
---
+++ src/tokenizer.rs
@@ -0,0 +1,164 @@
+use crate::str_checker;
+
+pub struct TokensResult<'a> {
+    pub is_valid_url: bool,
+
+    pub protocol: Option<&'a str>,
+    pub host: Option<&'a str>,
+    pub directories: Vec<&'a str>,
+    pub filename: Option<&'a str>,
+    pub query: Option<&'a str>,
+
+    pub words: Vec<&'a str>,
+    pub numbers: Vec<&'a str>,
+    pub alphanumeric: Vec<&'a str>,
+    pub punctuations: Vec<&'a str>,
+
+    pub tokens: Vec<&'a str>,
+    pub granular_tokens: Vec<&'a str>,
+}
+
+impl TokensResult<'_> {
+    pub fn granular(&self) -> String {
+        let mut granular_string = String::from("{\r\n");
+        for token in &self.granular_tokens {
+            granular_string.push_str(
+                format!(
+                    "    {} => {:?}\r\n",
+                    token,
+                    token.chars().into_iter().collect::<Vec<char>>()
+                )
+                .as_str(),
+            );
+        }
+        granular_string.push_str("}\r\n");
+        granular_string
+    }
+    pub fn new<'a>(input_str: &'a str) -> TokensResult<'a> {
+        let mut result = TokensResult {
+            is_valid_url: false,
+
+            protocol: None,
+            host: None,
+            directories: Vec::new(),
+            filename: None,
+            query: None,
+
+            words: Vec::new(),
+            numbers: Vec::new(),
+            alphanumeric: Vec::new(),
+            punctuations: Vec::new(),
+
+            tokens: tokenize(input_str),
+            granular_tokens: Vec::new(),
+        };
+
+        // Check if it's a URL for a website.
+        result.is_valid_url = result.tokens.len() >= 3
+            && is_valid_protocol(result.tokens.get(0).unwrap())
+            && !result.tokens.get(0).unwrap().is_empty()
+            && result.tokens.get(1).unwrap().is_empty();
+
+        if result.is_valid_url {
+            let protocol_str = result.tokens.get(0).unwrap();
+            result.protocol = Some(&protocol_str[..protocol_str.len() - 1]);
+            result.host = Some(result.tokens.get(2).unwrap());
+
+            // [protocol:]/[empty]/[host]
+            if result.tokens.len() > 3 {
+                let it = &mut result.tokens[3..].into_iter().peekable();
+                while let Some(token) = it.next() {
+                    // Check if it's the last element in the list of tokens.
+                    if it.peek().is_none() {
+                        let (filename, query) = split_query(token);
+                        result.filename = Some(filename);
+                        result.query = Some(query);
+                    } else {
+                        result.directories.push(token);
+                    }
+                }
+            } else {
+                let (host, query) = split_query(result.tokens.get(2).unwrap());
+                result.host = Some(host);
+                result.query = Some(query);
+            }
+        }
+
+        for token in &result.tokens {
+            if token.is_empty() {
+                continue;
+            }
+            let mut start_index = 0;
+            for (index, c) in token.chars().enumerate() {
+                // Split the token if a punctuation is encountered.
+                if !c.is_alphanumeric() {
+                    // Push the characters before the punctuation and the punctuation itself.
+                    result.granular_tokens.push(&token[start_index..index]);
+                    result.granular_tokens.push(&token[index..index + 1]);
+                    start_index = index + 1;
+                }
+            }
+            let last_part: &str = &token[start_index..];
+            result.granular_tokens.push(last_part);
+        }
+
+        for token in &result.granular_tokens {
+            if str_checker::is_number(token) {
+                result.numbers.push(token);
+            } else if str_checker::is_word(token) {
+                result.words.push(token);
+            } else if str_checker::is_alphanumeric(token) {
+                result.alphanumeric.push(token);
+            // Discard empty tokens.
+            } else if !token.is_empty() {
+                result.punctuations.push(token);
+            }
+        }
+
+        result
+    }
+}
+
+pub fn is_valid_protocol(protocol_str: &str) -> bool {
+    let end_index: usize = protocol_str.len() - 1;
+    for (current_index, c) in protocol_str.chars().enumerate() {
+        if (current_index != end_index && !c.is_alphabetic())
+            || (current_index == end_index && c != ':')
+        {
+            return false;
+        }
+    }
+    true
+}
+
+// Some url requests a query string after the filename.
+// --> for example: https://www.example.com/article/123?category=technology#section2
+// Separation should be done to properly extract the filename.
+pub fn split_query(mixed: &str) -> (&str, &str) {
+    let first_part: &str;
+    let query: &str;
+
+    for (index, c) in mixed.chars().enumerate() {
+        if !c.is_alphanumeric() && c != '.' {
+            first_part = &mixed[..index];
+            query = &mixed[index..];
+            return (first_part, query);
+        }
+    }
+    (mixed, "")
+}
+
+fn tokenize(input_str: &str) -> Vec<&str> {
+    let mut tokens: Vec<&str> = Vec::new();
+    let mut start_index: usize = 0;
+    for (index, c) in input_str.chars().enumerate() {
+        if c == '/' {
+            let extracted: &str = &input_str[start_index..index];
+            tokens.push(extracted);
+            start_index = index + 1;
+        }
+    }
+    let last_part: &str = &input_str[start_index..];
+    tokens.push(last_part);
+    tokens
+}
